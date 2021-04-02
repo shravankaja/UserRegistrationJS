4da44fafc65d216aee840cc82e7ea87ca3dc3cf4
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-02 21:05:39 +0530

    Added: implemented promise to prompt for last name after valiating first name

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,51 +1,65 @@
 console.log("Welcome To User Registration");
 
+const prompt = require('prompt-sync')()
+
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
+});
+
+let FirstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+let LASTNAMEPATTERN = "^[A-Z][a-zA-Z]{2,}$";
+let EMAILPATTERN = "^[a-zA-z0-9]{1,}([\\.\\_\\+\\-])?[a-zA-Z0-9]{0,}@[a-zA-z0-9]{1,}\\.[a-z]{2,3}(\\.)?([a-z]{2,3})?";
+let PHONEPATTERN = "^[9][1][0-9]{10}$";
+let PASSWORDPATTERN = "^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*])(?!.*[!@#$%^&*].*[!@#$%^&*]).*$";
+
+
+function testPattern(str, pattern) {
+  var patt = new RegExp(pattern);
+  var res = patt.test(str);
+  return res;
+}
+
+function check(name, pattern) {
+  let result = testPattern(name, pattern);
+  if (result == true) {
+    console.log("Entered name is correct");
+    return true;
+  }
+  else if (result == false) {
+    console.log("Please enter correct name");
+    return false;
+  }
+}
+
+let read = function (str) {
+  let value = prompt(str)
+  return value;
+}
+
+let validate = function (str, patternToCheck) {
+  return new Promise((resolve, reject) => {
+    let result;
+    setTimeout(() => {
+      let nameCheck = read(str)
+      result = check(nameCheck, patternToCheck);
+      while (result == false) {
+        nameCheck = read(str)
+        result = check(nameCheck, patternToCheck);
+        if (result == true) {
+          resolve('done')
+          break;
+        }
+
+      }
+
+    }, 100)
+
   });
+}
 
-  let  FirstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
-  let  LASTNAMEPATTERN = "^[A-Z][a-zA-Z]{2,}$";
-  let  EMAILPATTERN = "^[a-zA-z0-9]{1,}([\\.\\_\\+\\-])?[a-zA-Z0-9]{0,}@[a-zA-z0-9]{1,}\\.[a-z]{2,3}(\\.)?([a-z]{2,3})?";
-  let  PHONEPATTERN = "^[9][1][0-9]{10}$";
-  let  PASSWORDPATTERN = "^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*])(?!.*[!@#$%^&*].*[!@#$%^&*]).*$";
-    
-
-  function validate(str,pattern) {
-    var patt = new RegExp(pattern);
-    var res = patt.test(str);
-    return res;
-  }
-  
-  function check(name,pattern) {
-    let result = validate(name,pattern);
-       if(result==true) {
-           console.log("Entered name is correct");
-           return true;
-       }
-       else {
-           console.log("Please enter correct name");
-           return false;
-        }
-  }
-
- let read = function (str,patternToCheck) {
-        readline.question(str, function(nameToCheck) {
-        let result = check(nameToCheck,patternToCheck);
-        if(result==false){
-          read(str,patternToCheck)
-        }
-      });
-      return true;
- }
-
-read("Enter First Name :",FirstNamePattern)
+validate("Enter first name :",FirstNamePattern).then(()=>validate("Enter last name :",LASTNAMEPATTERN))
 
 
- 
-
- 
 
 
- 
