fd4ef9a0be4b7aea7694622fe5695039b54cf8b4
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-07 13:34:50 +0530

    Added: functions to be tested

---
+++ testUsingChai/utils.js
@@ -0,0 +1,31 @@
+module.exports ={
+      testPattern(str,pattern) {
+        var patt = new RegExp(pattern);
+        var res = patt.test(str);
+        let obj = {
+          name : str,
+          age : 24
+        }
+        if(res == true) {
+            return [str,obj,true];
+        }
+        else {
+          return [str,obj,false];
+        }
+    },
+     check(name, pattern) {
+        let result = module.exports.testPattern(name,pattern);
+        if (result == true) {
+          console.log("Validated");
+          return true;
+        }
+        else if (result == false) {
+          console.log("Please follow format");
+          return false;
+        }
+      },
+      read(str,prompt) {
+        let value = prompt(str)
+        return value;
+      }
+}
